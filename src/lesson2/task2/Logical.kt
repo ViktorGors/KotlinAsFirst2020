e82c9af6115f2c970607e7bb92c07c80f71c102b
ViktorGors <90250275+ViktorGors@users.noreply.github.com>
2021-09-12 21:58:54 +0300

    lesson2task2 "fun isNumberHappy" and "fun daysInMonth" сделаны

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,7 +18,11 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val numeral12 = (number / 1000) + ((number / 100) % 10)
+    val numeral34 = ((number / 10) % 10) + (number % 10)
+    return if (numeral12 == numeral34) true else false
+}
 
 /**
  * Простая (2 балла)
@@ -36,7 +40,25 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    var February : Int
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) February = 29 else February = 28
+    //return if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) 31
+    return when (month) {
+        1 -> 31
+        3 -> 31
+        4 -> 30
+        5 -> 31
+        6 -> 30
+        7 -> 31
+        8 -> 31
+        9 -> 30
+        10 -> 31
+        11 -> 30
+        12 -> 31
+        else -> February
+    }
+}
 
 /**
  * Простая (2 балла)
