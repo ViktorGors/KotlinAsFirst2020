777dc950d5f85877e74ba2ae59df33556b2dccc0
ViktorGors <90250275+ViktorGors@users.noreply.github.com>
2021-09-12 21:58:54 +0300

    lesson2task2 "fun isNumberHappy" and "fun daysInMonth" сделаны

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -68,7 +68,14 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    return when {
+        age % 10 == 1 -> "$age год"
+        age / 10 % 10 == 1 -> "$age лет"
+        age / 10 % 10 == 9 -> "$age лет"
+        else -> "$age года"
+    }
+}
 
 /**
  * Простая (2 балла)
@@ -77,11 +84,28 @@ fun ageDescription(age: Int): String = TODO()
  * и t3 часов — со скоростью v3 км/час.
  * Определить, за какое время он одолел первую половину пути?
  */
+//в процесе
 fun timeForHalfWay(
     t1: Double, v1: Double,
     t2: Double, v2: Double,
-    t3: Double, v3: Double
-): Double = TODO()
+    t3: Double, v3: Double,
+): Double {
+    val s1 = t1 * v1
+    val s2 = t2 * v2
+    val s3 = t3 * v3
+    var T: Double
+    val ForHalfWay = (s1 + s2 + s3) / 2
+    var k1: Double
+    var k2: Double
+    var k3: Double
+    if (s1 == ForHalfWay) T =+ t1 else T =+ 0.0
+    if (s2 == ForHalfWay) T =+ t2 else T =+ 0.0
+    if (s3 == ForHalfWay) T =+ t3 else T =+ 0.0
+
+
+    return T
+}
+
 
 /**
  * Простая (2 балла)
