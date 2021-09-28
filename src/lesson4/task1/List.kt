d3da5fb77c7d347176602ebfede5240b9c1f9aa8
ViktorGors <90250275+ViktorGors@users.noreply.github.com>
2021-09-28 20:25:17 +0300

    перенос

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -120,14 +120,20 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double {
+    var summa = 0.0
+    v.forEach {el -> summa += (el * el)}
+    return sqrt(summa)
+}
+
 
 /**
  * Простая (2 балла)
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+// .isEmpty - пустой
+fun mean(list: List<Double>): Double = if (list.isEmpty()) 0.0 else list.sum() / list.size
 
 /**
  * Средняя (3 балла)
