9c334847723db4b33440a7f1188faca993c68dec
ViktorGors <90250275+ViktorGors@users.noreply.github.com>
2021-11-07 18:20:58 +0300

    l5

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -168,8 +168,13 @@ fun whoAreInBoth(a: List<String>, b: List<String>): List<String> {
  *     mapOf("Emergency" to "911", "Police" to "02")
  *   ) -> mapOf("Emergency" to "112, 911", "Police" to "02")
  */
-fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> = TODO()
-
+fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> {
+    var mapC = mapA.toMutableMap()
+    for ((name, number) in mapB)
+        if ((name in mapC) && (mapC[name] != number)) mapC[name] += ", $number"
+        else mapC += name to number
+    return mapC
+}
 /**
  * Средняя (4 балла)
  *
