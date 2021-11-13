758a31b650827bc121d76d4f16055a711f6eb450
ViktorGors <90250275+ViktorGors@users.noreply.github.com>
2021-11-13 18:49:25 +0300

    l6 fix

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -76,15 +76,7 @@ fun main() {
  * Обратите внимание: некорректная с точки зрения календаря дата (например, 30.02.2009) считается неверными
  * входными данными.
  *
- * fun timeStrToSeconds(str: String): Int {
-val parts = str.split(":")
-var result = 0
-for (part in parts) {
-val number = part.toInt()
-result = result * 60 + number
-}
-return result
-}
+ *
  */
 fun dateStrToDigit(str: String): String {
     val mon = listOf(
