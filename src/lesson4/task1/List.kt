dda580a345d18ad6f60f10bdd9a8ce980306e3b8
ViktorGors <90250275+ViktorGors@users.noreply.github.com>
2021-11-13 18:59:41 +0300

    l4 fix

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -275,6 +275,10 @@ fun factorizeToString(n: Int): String = factorize(n).joinToString(separator = "*
  * Результат перевода вернуть в виде списка цифр в base-ичной системе от старшей к младшей,
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
+
+fun convert(n: Int, base: Int): List<Int> = TODO()
+/**
+ * Ошибку наконец-то понял-увидел. Работаю над исправлением.
 fun convert(n: Int, base: Int): List<Int> {
     val result = mutableListOf<Int>()
     var N = n
@@ -284,6 +288,7 @@ fun convert(n: Int, base: Int): List<Int> {
     }
     return result
 }
+ */
 
 /**
  * Сложная (4 балла)
@@ -296,6 +301,10 @@ fun convert(n: Int, base: Int): List<Int> {
  * Использовать функции стандартной библиотеки, напрямую и полностью решающие данную задачу
  * (например, n.toString(base) и подобные), запрещается.
  */
+fun convertToString(n: Int, base: Int): String = TODO()
+
+/**
+ * Активно работаю над исправлением проблемы.
 fun convertToString(n: Int, base: Int): String {
     val alphabet =
         listOf(
@@ -309,6 +318,7 @@ fun convertToString(n: Int, base: Int): String {
         transform = { if (it < 10) "$it" else alphabet[it - 10].toString() }
     )
 }
+ */
 
 /**
  * Средняя (3 балла)
