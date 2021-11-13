55df675ca4727494693f4cd86632411e22db45d9
ViktorGors <90250275+ViktorGors@users.noreply.github.com>
2021-11-13 15:58:55 +0300

    l4 fix

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -276,13 +276,13 @@ fun factorizeToString(n: Int): String = factorize(n).joinToString(separator = "*
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
 fun convert(n: Int, base: Int): List<Int> {
-    var list = mutableListOf<Int>()
+    val list = mutableListOf<Int>()
     var N = n
     while (N != 0) {
         list.add(N % base)
         N /= base
     }
-    list = list.asReversed()
+    list.reverse()
     return list
 }
 
